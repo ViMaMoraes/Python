9247193ccfecf35c8231c3e0cdda8fe7396bacbf
ViMaMoraes <mvmoraes81@gmail.com>
2022-11-03 22:32:27 -0300

    Pitão

---
+++ Estrutura de Dados/LIstas.py
@@ -0,0 +1,18 @@
+# ----------------------------------------------------\
+# Armazenar mais de uma informação em variáveis
+# Manter a sequencia dos dados em uma variável
+
+cidade1 = 'Rio de Janeiro'
+cidade2 = 'São Paulo'
+cidade3 = 'Salvador'
+
+cidades = ['Rio de Janeiro', 'São Paulo', 'Salvador', 'Goiana']
+#                  0              1           2           3
+
+# cidades.append('Santa Catarina')
+# cidades.remove('Salvador')
+# cidades.insert(1, 'Santa Catarina')
+# cidades.pop(0)
+# cidades.sort()
+
+print(cidades)
